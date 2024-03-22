3c3fe24e22542370d1a9cff0ad417c830af0d926
RedJohn22 <162618988+RedJohn22@users.noreply.github.com>
2024-03-22 04:31:33 +0200

    Update

---
+++ jstask.js
@@ -0,0 +1,43 @@
+var Number = +prompt("Enter the number");
+if (Number > 0 ) {
+    console.log ('number is negative')
+    
+} else if (Number < 0) {
+    console.log ('number is positive')
+    
+} else {
+    console.log('number is neutral')
+}
+
+var Number = +prompt("Enter the number");
+if(Number % 2 == 0) {
+    console.log ("The number is even.");
+} else {
+    console.log ("The number is odd.");
+}
+
+var num1 = +prompt("Enter the number");
+var num2 = +prompt("Enter the number");
+if (num1 == num2) {
+    console.log ("both are equal");
+} else if (num1 > num2) {
+   console.log (num1 + " is greater");
+
+} else {
+   console.log (num2 + " is greater");
+}
+
+var Grade = +prompt("Enter the number");
+if (Grade <= 100 && grade >= 90) {
+    consloe.log ('الله اكبر عليك ايه الحلاوة دي')
+    
+} else if (Grade <= 90 && grade >= 80 ){
+    console.log ('يا سلااااام')
+} else if (Grade <= 80 && grade >= 70) {
+    console.log ('يعني مش أوي')
+    
+} else if (grade < 70 && grade >= 60) {
+    console.log ('الحمدلله ما غرق')
+} else {
+    console.log (احمس)
+}
